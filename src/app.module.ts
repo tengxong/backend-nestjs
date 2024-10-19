9b85e0bb587ddc13d7ac86bc0ab5c893f04b0836
tengxong <119726458+tengxong@users.noreply.github.com>
2024-10-19 10:14:44 +0700

    'update'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
+  imports: [DatabaseModule],
 })
 export class AppModule {}
